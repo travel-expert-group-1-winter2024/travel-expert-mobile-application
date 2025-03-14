29cc6a99cccb719d8a424545f3e4d173c423982b
James <st.sorawat38@gmail.com>
2025-03-14 16:48:45 -0600

    Create temporary login page (#3)
    * [build] add initial deployment and VCS configuration files
    
    * [doc] create readme
    
    * [feat] create simple login page
    
    * [fix] rename textView6 to tvSignUp in activity_login.xml
    
    * [build] set login to be launcher
    
    * [feat] init login activity
    
    * [feat] adjust layout margins in activity_login.xml
    
    * [feat] initialize views in LoginActivity
    
    * [feat] add checking username and password

---
+++ app/src/main/java/com/example/travelexpertmobileapplication/LoginActivity.java
@@ -0,0 +1,58 @@
+package com.example.travelexpertmobileapplication;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class LoginActivity extends AppCompatActivity {
+
+    EditText etUsername, etPassword;
+    Button btnLogin;
+    TextView tvSignUp;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_login);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        // Initialize the views
+        etUsername = findViewById(R.id.etLoginEmail);
+        etPassword = findViewById(R.id.etLoginPassword);
+        btnLogin = findViewById(R.id.btnLogin);
+        tvSignUp = findViewById(R.id.tvSignUp);
+
+        // Set on click listener on the sign up text view
+        btnLogin.setOnClickListener(v -> {
+            // Get the username and password from the edit text fields
+            String username = etUsername.getText().toString();
+            String password = etPassword.getText().toString();
+            // Check if the username and password are empty
+            if (username.isEmpty() || password.isEmpty()) {
+                // Show an error message
+                Toast.makeText(LoginActivity.this, "Please enter your username and password", Toast.LENGTH_SHORT).show();
+            } else {
+                // Create an intent to start the main activity
+                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                // Start the main activity
+                startActivity(intent);
+                // Finish the current activity
+                finish();
+            }
+        });
+    }
+}
