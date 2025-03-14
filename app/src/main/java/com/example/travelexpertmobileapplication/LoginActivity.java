016231d871bad30595a05a6f198382f795a404dd
sorawat38 <st.sorawat38@gmail.com>
2025-03-14 16:42:18 -0600

    [feat] initialize views in LoginActivity

--- app/src/main/java/com/example/travelexpertmobileapplication/LoginActivity.java
+++ app/src/main/java/com/example/travelexpertmobileapplication/LoginActivity.java
@@ -1,6 +1,9 @@
 package com.example.travelexpertmobileapplication;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +12,10 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class LoginActivity extends AppCompatActivity {
+
+    EditText etUsername, etPassword;
+    Button btnLogin;
+    TextView tvSignUp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +27,11 @@ public class LoginActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        // Initialize the views
+        etUsername = findViewById(R.id.etLoginEmail);
+        etPassword = findViewById(R.id.etLoginPassword);
+        btnLogin = findViewById(R.id.btnLogin);
+        tvSignUp = findViewById(R.id.tvSignUp);
     }
 }
