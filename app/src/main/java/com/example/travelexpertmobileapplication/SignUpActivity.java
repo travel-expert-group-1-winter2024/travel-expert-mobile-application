8c3de43467db6da7cf9281522fff29db27fecee1
sorawat38 <st.sorawat38@gmail.com>
2025-03-16 01:02:54 -0600

    [refactor] rename last name EditText ID for clarity

--- app/src/main/java/com/example/travelexpertmobileapplication/SignUpActivity.java
+++ app/src/main/java/com/example/travelexpertmobileapplication/SignUpActivity.java
@@ -1,6 +1,10 @@
 package com.example.travelexpertmobileapplication;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.Spinner;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,6 +16,12 @@ public class SignUpActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        EditText etFirstName, etMiddleInitial, etLastName, etPhoneNumber, etEmail;
+        Spinner spinnerAgency;
+        Button btnSubmit;
+        ImageButton btnBack;
+
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_sign_up);
@@ -20,5 +30,19 @@ public class SignUpActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        // Initialize UI components
+        etFirstName = findViewById(R.id.etFirstName);
+        etMiddleInitial = findViewById(R.id.etMiddleInitial);
+        etLastName = findViewById(R.id.etLastName);
+        etPhoneNumber = findViewById(R.id.etPhoneNumber);
+        etEmail = findViewById(R.id.etEmail);
+        spinnerAgency = findViewById(R.id.spnAgency);
+        btnSubmit = findViewById(R.id.btnSubmit);
+        btnBack = findViewById(R.id.btnBack);
+
+        // Back Button Click
+        btnBack.setOnClickListener(v -> finish());
+
     }
 }
