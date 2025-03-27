a17b5a94a25ee69c7eabcfa3a112ba2d2cf8a886
sorawat38 <st.sorawat38@gmail.com>
2025-03-27 00:03:40 -0600

    [feat] remove default log

--- app/src/main/java/com/example/travelexpertmobileapplication/LoginActivity.java
+++ app/src/main/java/com/example/travelexpertmobileapplication/LoginActivity.java
@@ -25,7 +25,6 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_login);
-        Timber.i("Timber logging is ready");
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
