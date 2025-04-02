2aebf160e7275f0671988aba4f8e044a64deaede
elahev <elahe.vafai@gmail.com>
2025-04-01 19:56:39 -0600

    create fragment_product_details.xml

--- app/src/main/java/com/example/travelexpertmobileapplication/network/api/ProductAPIService.java
+++ app/src/main/java/com/example/travelexpertmobileapplication/network/api/ProductAPIService.java
@@ -1,11 +1,18 @@
 package com.example.travelexpertmobileapplication.network.api;
 
+import com.example.travelexpertmobileapplication.model.Product;
 import com.google.gson.JsonArray;
 
 import retrofit2.Call;
+import retrofit2.http.Body;
 import retrofit2.http.GET;
+import retrofit2.http.PUT;
+import retrofit2.http.Path;
 
 public interface ProductAPIService {
     @GET("/api/products")
     Call<JsonArray> getProducts();
+
+    @PUT("/api/products/{id}")
+    Call<Product> updateProduct(@Path("id") int id, @Body Product product);
 }
